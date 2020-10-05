e299c77f87c242d8b68a93f26648bdca368c707e
Joe Jacques <joejacques@c02sy1fth03q.home>
2020-10-05 14:06:32 +0100

    task 2

---
+++ task2/task2.sh
@@ -0,0 +1,21 @@
+#!/bin/sh 
+
+
+// 1 
+
+
+number_one = 22
+number_two = 24
+
+remain = 73 % 8 
+
+echo $REMAIN 
+
+
+echo '4^6 + 5^3'
+
+if [ $NUMBER_ONE -gt $NUMBER_TWO] 
+then
+    echo $NUMBER_ONE 'IS LARGER'
+else
+    echo $NUMBER_TWO 'IS LARGER OR THEY ARE THE SAME'
