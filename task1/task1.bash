8817a04fccb5fa73fc17f08c04a4b2416f68c7f3
Joe Jacques <joejacques@c02sy1fth03q.home>
2020-10-05 12:35:11 +0100

    Task1 Bash file

---
+++ task1/task1.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+ name = 'Joe Jacques'
+  
+echo $SHELLOPTS
+
+
+echo $1
+echo 'Hello /$ how are you /! doing today /{ ' 
+
+touch my/specialfile > echo 'file created' 
+
+rm my/specialfile | echo 'file removed' 
