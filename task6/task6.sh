74094897f0527ddff9d89a61983f106d4f4137cc
Joe Jacques <joejacques@c02sy1fth03q.home>
2020-10-07 16:06:47 +0100

    feat: adding second part

--- task6/task6.sh
+++ task6/task6.sh
@@ -3,8 +3,17 @@
 # declare array 
 
 word_list=('this' 'is' 'an' 'array' 'and' 'there' 'are' 'many' 'items' 'here')
+websites=(localhost localhost:8080 localhost:5050)
 
 # Replace character in array 
 
 echo ${word_list[@]} | tr t T  
+
+input_urls=($1 $2 $3)
+
+
+for i in "${websites[@]}"; do
+    ping -c 4 $i | tail -1 | awk -F '/' '{print  $5}'
+done
+
 
