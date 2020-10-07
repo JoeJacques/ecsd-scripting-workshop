782cce9e6b7052bf30b7ae7de750401f33632b00
Joe Jacques <joejacques@c02sy1fth03q.home>
2020-10-07 15:03:21 +0100

    feat: adding second task 5 loop

--- task5/task5.sh
+++ task5/task5.sh
@@ -4,11 +4,12 @@
 # Create a while loop that asks for a password from a user. 
 
 password='123456'
-attempts=0
+attempts='0'
 
 #1 Create a while loop that requests a password from a user. 
 
-while [$attempts -lt 10 ]; do 
+while [$attempts -le 10]
+do 
     read user_password 
     if [$user_password -ne $password]; then 
         echo 'Password invalid'; 
@@ -17,7 +18,10 @@ while [$attempts -lt 10 ]; do
         echo 'password correct, access granted'
         break
     fi 
-    done 
+done 
 
+#2 create a loop that sums numbers. 
 
-exit 
+for i in {1..3}; do
+    echo $(($i+$i))
+done
