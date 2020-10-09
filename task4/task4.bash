2eb4dff2ef5904a58a7d9c90ac4f49790c8e3152
JoeJacques <joe.lewis.jacques@gmail.com>
2020-10-09 16:23:38 +0100

    fix: fixing task 4 error

--- task4/task4.bash
+++ task4/task4.bash
@@ -3,13 +3,11 @@
 # wc -w 
 
 time_now=$(date +%H)
-senetence=
+
 
 if (( "$time_now" >= "12" )) && (($time_now < "14")); then 
   echo 'TIME FOR LUNCH'
 else 
   echo 'LUNCH TIME IS OVER'
 fi 
-
-if (())
 
